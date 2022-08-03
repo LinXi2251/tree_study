c843a7704b71c0e24ff1e1b7a05afa6b57646147
Linxi2251 <2251998552@qq.com>
2022-08-03 10:31:39 +0800

    并查集over

--- main.cpp
+++ main.cpp
@@ -19,7 +19,12 @@ int union_vertices(int x, int y, int parent[], int rank[]) {
 
     if (x_root != y_root) {
         if (rank[x_root] > rank[y_root]) {
-
+            parent[y_root] = x_root;
+        } else if (rank[x_root] < rank[y_root]) {
+            parent[x_root] = y_root;
+        } else {
+            parent[y_root] = x_root;
+            rank[x_root]++;
         }
         return 1;
     } else {
@@ -35,10 +40,12 @@ int main() {
     int rank[6];
     memset(parent, -1, sizeof(int) * 6);
     memset(rank, 0, sizeof(int) * 6);
+    display(parent, 6);
+    display(rank, 6);
     for (int i = 0; i < 5; i++) {
         union_vertices(vertices[i][0], vertices[i][1], parent, rank);
     }
     display(parent, 6);
-    cout << "\n";
+    display(rank, 6);
     return 0;
 }
